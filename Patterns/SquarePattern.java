1915dc876cab44c06eda67375053be9bf2e116d8
ankit-gupta-00 <112970191+ankit-gupta-00@users.noreply.github.com>
2022-11-11 14:20:42 +0530

    Square Pattern

---
+++ Patterns/SquarePattern.java
@@ -0,0 +1,42 @@
+// You are given a number n.
+// You've to create a pattern of * as shown in output format.
+
+// Input Format :
+
+// A number n
+
+// Output Format :
+
+// *****
+// *****
+// *****
+// *****
+// *****
+
+
+import java.util.Scanner;
+public class SquarePattern
+{
+    public static Scanner scn = new Scanner(System.in);
+
+    // Square Pattern.
+    public static void squarePattern(int n)
+    {
+        int nst = n;
+        for(int row = 1; row <= n; row++)
+        {
+            for(int cst = 1; cst <= nst; cst++)
+            {
+                System.out.print("*");
+            }
+            nst = n;
+            System.out.println();
+        }
+    }
+    
+    public static void main(String[] args)
+    {
+        int n = scn.nextInt();
+        squarePattern(n);
+    }
+}
