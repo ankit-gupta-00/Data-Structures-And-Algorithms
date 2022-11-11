7989b4b346b8ef90a96de59116ca0b6379e4f17a
ankit-gupta-00 <112970191+ankit-gupta-00@users.noreply.github.com>
2022-11-11 14:50:52 +0530

    Number Diamond Pattern 1

---
+++ Patterns/NumberDiamondPattern1.java
@@ -0,0 +1,71 @@
+// You are given a number n.
+// You've to create a pattern of * as shown in output format.
+
+// Input Format :
+
+// A number n
+
+// Output Format :
+
+//     1
+//    222
+//   33333
+//  4444444
+// 555555555
+//  4444444
+//   33333
+//    222
+//     1
+
+// Constraints : 
+
+// n is Odd.
+
+
+import java.util.Scanner;
+public class NumberDiamondPattern1
+{
+    public static Scanner scn = new Scanner(System.in);
+
+    // Number Diamond Pattern.
+    public static void numberDiamondPattern(int n)
+    {
+        int nsp = n / 2;
+        int nst = 1;
+        for(int row = 1; row <= n; row++)
+        {
+            for(int csp = 1; csp <= nsp; csp++)
+            {
+                System.out.print(" ");
+            }
+            int val = row;
+            if(row > (n / 2) + 1)
+            {
+                val = n - row + 1;
+            }
+            for(int cst = 1; cst <= nst; cst++)
+            {
+                System.out.print(val);
+            }
+            val++;
+            if(row <= (n / 2))
+            {   
+                nsp--;
+                nst += 2;
+            }
+            else
+            {
+                nsp++;
+                nst -= 2;
+            }
+            System.out.println();
+        }
+
+    }
+
+    public static void main(String[] args)
+    {
+        int n = scn.nextInt();
+        numberDiamondPattern(n);
+    }
+}
