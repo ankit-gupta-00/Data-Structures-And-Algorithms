c7f1293f71831dc8f13a2ed3252e9f76e3849656
Ankit Gupta <112970191+ankit-gupta-00@users.noreply.github.com>
2022-11-13 23:32:25 +0530

    Right Triangle Pascal Pattern

---
+++ Patterns/RightTrianglePascalPattern.java
@@ -0,0 +1,20 @@
+// You are given a number n.
+// You've to create a pattern as shown in output format.
+
+// Input Format :
+
+// A number n
+
+// Output Format :
+
+// 1
+// 1   1
+// 1   2   1
+// 1   3   3   1
+// 1   4   6   4   1
+// 1   5   10  10  5   1
+// ...
+
+// Constraints :
+
+// 1 <= n <= 10
