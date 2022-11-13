d3cb7a27d59680c3ffdd1813fd669c6175be96c0
Ankit Gupta <112970191+ankit-gupta-00@users.noreply.github.com>
2022-11-13 23:31:24 +0530

    Hill Number Pattern

---
+++ Patterns/HillNumberPattern.java
@@ -0,0 +1,18 @@
+// You are given a number n.
+// You've to write code to print the pattern given in output format below.
+
+// Input Format :
+
+// A number n
+
+// Output Format :
+
+// 1     1
+// 12   21
+// 123 321
+// 1234321
+
+// Constraints : 
+
+// 1 <= n <= 10
+
